12d25fc3c5971cdd668e0797eac098827e7a000f
Marc Monforte <marcjmonforte@gmail.com>
2018-05-20 22:21:50 -0700

    Fixed logic

--- ESPPcalculator.py
+++ ESPPcalculator.py
@@ -1,12 +1,19 @@
 import sys
 
-annual_salary = float(sys.argv[1])
-estimated_stock_price = float(sys.argv[2])
-max_shares_allowed = int(sys.argv[3])
+annual_salary = float(input("What is your annual salary?	"))
+estimated_stock_price = float(input("What is the estimated stock price?	"))
+max_shares_allowed = int(input("What is the max number of shares allowed?	"))
+pay_rate = str(input("Are you paid on a monthly or bimonthly rate?	"))
 
-salary_per_pay_period = annual_salary / 12
+if pay_rate == "monthly":
+	pay_rate_value = 12
+elif pay_rate == "bimonthly":
+	pay_rate_value = 24
+
+salary_per_pay_period = annual_salary / pay_rate_value
 discounted_stock_price = estimated_stock_price * 0.85
 max_total_purchase_price = discounted_stock_price * max_shares_allowed
 total_salary_collected_over_plan = salary_per_pay_period * 12
 
-print(max_total_purchase_price / total_salary_collected_over_plan)
+suggested_contribution = max_total_purchase_price / total_salary_collected_over_plan
+print("\nSuggested percent contribution of salary per pay period:	" + str(suggested_contribution))
